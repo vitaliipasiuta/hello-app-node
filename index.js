6802a688e5fefa9f341a71df6e1ba90be3ba90c8
vitalii.pasiuta <vitalii.pasiuta@ukr-vitaliipasiuta.local>
2020-05-25 22:54:43 +0300

    az

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const axios = require('axios');
+const os = require('os');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hi!'));
+const azURL = 'http://169.254.169.254/latest/meta-data/placement/availability-zone';
+const hostname = os.hostname();
+
+app.get('/', (req, res) => {
+    axios.get(azURL)
+        .then(response => {
+            res.send(`Hi from ${hostname} AZ: ${response.data}!`);
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
